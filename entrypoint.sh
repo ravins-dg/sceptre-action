d688125863419f3687ae0f15aef2968fd4611532
Ravisankar Sivasankaran <Ravisankar.Sivasankaran@domesticandgeneral.com>
2023-03-17 18:40:27 +0530

    Remove troposphere installation

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,42 +28,9 @@ function parseInputs {
     sceptreDir=${INPUT_SCEPTRE_DIRECTORY}
   fi
 
-  sceptreTroposphere=0
-  if [[ "${INPUT_SCEPTRE_TROPOSPHERE}" != "" ]]; then
-    if [[ "${INPUT_SCEPTRE_TROPOSPHERE}" == "false" ]]; then
-      sceptreTroposphere=0
-    elif [[ "${INPUT_SCEPTRE_TROPOSPHERE}" == "true" ]]; then
-      sceptreTroposphere=1
-    else
-      echo "Invalid input for sceptre_troposphere, must be 'true' or 'false'"
-      exit 1
-    fi
-  else
-      echo "Input sceptre_troposphere cannot be empty!"
-      exit 1
-  fi
-
-  sceptreTroposphereVer='2.6.0'
-  if [[ "${INPUT_SCEPTRE_TROPOSPHERE_VERSION}" != "" ]]; then
-    if [[ "${INPUT_SCEPTRE_TROPOSPHERE_VERSION}" =~ ^2[.][0-9][.][0-9]$ ]]; then
-      sceptreTroposphereVer=${INPUT_SCEPTRE_TROPOSPHERE_VERSION}
-    else
-      echo "Unsupported troposphere version, must be >2.0.0"
-      echo "Version entered: ${INPUT_SCEPTRE_TROPOSPHERE_VERSION}"
-      exit 1
-    fi
-  else
-    echo "Input sceptre_troposphere_version cannot be empty!"
-    exit 1
-  fi
-
 }
 
 function installDeps {
-  if [[ "${sceptreTroposphere}" == 1 ]]; then
-    echo "Installing Troposphere"
-    pip install --no-input troposphere==$sceptreTroposphereVer
-  fi
   echo "Installing Sceptre"
   pip install --no-input sceptre==$sceptreVer
 }
